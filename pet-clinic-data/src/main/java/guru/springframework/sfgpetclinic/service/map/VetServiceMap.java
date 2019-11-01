fdfe0cf704d64baa3b0b0854d5fdf8b3774a6b48
Krzysztof Kukla <krzys.kukla@gmail.com>
2019-11-02 00:44:37 +0100

    69 implement map services. closes #6

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.service.map;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.service.VetService;
+
+/**
+ * @author Krzysztof Kukla
+ */
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+}
