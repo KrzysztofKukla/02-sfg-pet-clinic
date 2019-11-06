61fefd103fb7afee08fd1ed3db832156bb7afa30
Krzysztof Kukla <krzys.kukla@gmail.com>
2019-11-06 17:24:00 +0100

    87 load data on startup

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -2,12 +2,14 @@ package guru.springframework.sfgpetclinic.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 /**
  * @author Krzysztof Kukla
  */
 @Getter
 @Setter
+@ToString
 public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
