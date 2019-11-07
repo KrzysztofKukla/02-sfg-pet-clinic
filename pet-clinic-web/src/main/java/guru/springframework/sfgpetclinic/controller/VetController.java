f0872de567f98b3a1aae64eae14e5e877e29f5cd
Krzysztof Kukla <krzys.kukla@gmail.com>
2019-11-07 10:53:47 +0100

    90 implement vets list

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgpetclinic.controller;
 
+import guru.springframework.sfgpetclinic.service.VetService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -8,9 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("/vets")
+@RequiredArgsConstructor
 public class VetController {
+    private final VetService vetService;
+
     @RequestMapping({"", "/index"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 
