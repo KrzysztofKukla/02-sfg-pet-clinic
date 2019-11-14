15090da412e04a9a57edde68b88e3b057a46f2ac
Krzysztof Kukla <krzys.kukla@gmail.com>
2019-11-14 15:57:03 +0100

    152 Base Entity implementation

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -3,15 +3,25 @@ package guru.springframework.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * @author Krzysztof Kukla
  */
+@MappedSuperclass
 @Getter
 @Setter
 public class BaseEntity implements Serializable {
 
+    @Id
+    //IDENTITY is a special type that will support the automatic generation of a sequence
+    // for example MySql, not provided in each ones of databases
+    // added in Oracle 12
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     //box type is recommended by Hibernate
     private Long id;
 
